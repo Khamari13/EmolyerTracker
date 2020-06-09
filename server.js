89d947c7fa9e94f66b2af2814d7e73c20e3be059
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-06-09 18:43:35 -0400

    added function to prompt users options

--- server.js
+++ server.js
@@ -12,3 +12,27 @@ const connection = mysql.createConnection({
     password: "rootroot",
     database: "..."
 })
+
+connection.connect(function(err) {
+    if (err) throw (err)
+    initApp()
+})
+
+const initApp = function() {
+    inquirer
+    .prompt({
+        name: "option",
+        message: "choose action",
+        type: "rawlist",
+        choices: [
+            "view Departments",
+            "view Roles",
+            "view Employees",
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "Update Employee role",
+            "Exit"
+        ]
+    })
+}
