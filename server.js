8a7d7c74a2c21fdd79ffa2768573825846b4917d
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-06-09 19:04:33 -0400

    added cases to the function

--- server.js
+++ server.js
@@ -35,4 +35,39 @@ const initApp = function() {
             "Exit"
         ]
     })
+    .then(function(choice) {
+        switch (choice.option){
+            case "view Departments":
+                viewDepartments()
+                break
+
+                case "view Roles":
+                    viewRoles()
+                    break
+
+                case "view Employees":
+                    viewEmployees()
+                    break
+
+                case "Add Department":
+                    addDepartment()
+                    break
+
+                case "Add Role":
+                    addRole()
+                    break
+
+                case "Add Employee":
+                    addEmployee()
+                    break
+
+                case "Update Employee role":
+                    updateEmployee()
+                    break
+
+                case "Exit":
+                    connection.end()
+                    break
+        }
+    })
 }
